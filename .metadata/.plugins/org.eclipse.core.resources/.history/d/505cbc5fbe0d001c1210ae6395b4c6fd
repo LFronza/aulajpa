526b4874722d742e7f4bce601e34e95b1f1cebee
Leonardo Fronza <lfronza312@gmail.com>
2021-09-04 18:26:37 -0300

    Primeiro Commit

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/d/505cbc5fbe0d001c1210ae6395b4c6fd
@@ -0,0 +1,18 @@
+package aplicacao;
+
+import dominio.Pessoa;
+
+public class Programa {
+
+	public static void main(String[] args) {
+
+		Pessoa p1 = new Pessoa(1, "Leonardo Fronza", "lfronza@email.com");
+		Pessoa p2 = new Pessoa(2, "João da Silva", "jsilva@email.com");
+		Pessoa p3 = new Pessoa(3, "Pedro de Oliveira", "poliveira@email.com");
+		
+		System.out.println(p1);
+		System.out.println(p2);
+		System.out.println(p3);
+	}
+
+}
---
+++ aulajpamaven/src/main/java/aplicacao/Programa.java
@@ -0,0 +1,44 @@
+package aplicacao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import dominio.Pessoa;
+
+public class Programa {
+
+	public static void main(String[] args) {
+
+//		Pessoa p1 = new Pessoa(null, "Leonardo Fronza", "lfronza@email.com");
+//		Pessoa p2 = new Pessoa(null, "João da Silva", "jsilva@email.com");
+//		Pessoa p3 = new Pessoa(null, "Pedro de Oliveira", "poliveira@email.com");
+
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("projeto-jpa");
+
+		EntityManager em = emf.createEntityManager();
+
+//		em.getTransaction().begin();
+//		em.persist(p1);
+//		em.persist(p2);
+//		em.persist(p3);
+//		em.getTransaction().commit();
+
+//		Pessoa p = em.find(Pessoa.class, 1);
+//
+//		System.out.println(p);
+		
+		em.getTransaction().begin();
+		Pessoa p = em.find(Pessoa.class, 2);
+		em.remove(p);
+		em.getTransaction().commit();
+		
+
+		System.out.println("OK!");
+		
+		em.clear();
+		emf.close();
+
+	}
+
+}
